b0050377d1e79dbe902ac0db3c953ba5379277a5
Irene García <irenegwodak@gmail.com>
2023-10-16 11:17:59 +0200

    Add filter to select continent

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ function App() {
   // state
   const [countries, setCountries] = useState([]);
   const [searchValue, setSearchValue] = useState('');
+  const [continentSelect, setContinentSelect] = useState('all');
 
   //effects
   useEffect(() => {
@@ -15,7 +16,9 @@ function App() {
   }, []);
 
   //events
+  const handleFormSubmit = (ev) => ev.preventDefault();
   const handleSearchInput = (ev) => setSearchValue(ev.currentTarget.value);
+  const handleSelectContinent = (ev) => setContinentSelect(ev.target.value);
   //renders
   const renderHeader = () => {
     return (
@@ -30,19 +33,30 @@ function App() {
   };
   const renderCountryCards = () => {
     return countries
-    .filter((country) => {
-      return country.name.toLowerCase().includes(searchValue.toLowerCase());
-    })
-    .map((country) => {
-      return (
-        <li key={country.id}>
-          <p>{country.flag}</p>
-          <p>{country.name}</p>
-          <p>{country.capital}</p>
-          <p>{country.continents}</p>
-        </li>
-      );
-    });
+      .filter((country) => {
+        return country.name.toLowerCase().includes(searchValue.toLowerCase());
+      })
+      .filter((country) => {
+        if (continentSelect === 'all') {
+          return country;
+        } else if (continentSelect === 'north-america') {
+          return country.continents === 'North America';
+        } else if (continentSelect === 'south-america') {
+          return country.continents === 'South America';
+        } else {
+          return country.continents.toLowerCase() === continentSelect;
+        }
+      })
+      .map((country) => {
+        return (
+          <li key={country.id}>
+            <p>{country.flag}</p>
+            <p>{country.name}</p>
+            <p>{country.capital}</p>
+            <p>{country.continents}</p>
+          </li>
+        );
+      });
   };
 
   return (
@@ -50,24 +64,33 @@ function App() {
       {renderHeader()}
       <main>
         <div>
-          <p>Filters</p>
-          <input
-            type="text"
-            name="search"
-            id="search"
-            placeholder="Busca por nombre"
-            value={searchValue}
-            onChange={handleSearchInput}
-          />
-          <select name="" id="">
-            <option value="all">All</option>
-            <option value="africa">Africa</option>
-            <option value="north-america">North America</option>
-            <option value="south-america">South America</option>
-            <option value="asia">Asia</option>
-            <option value="europe">Europe</option>
-            <option value="oceania">Oceania</option>
-          </select>
+          <form action="" onSubmit={handleFormSubmit}>
+            <p>Filters</p>
+            <label htmlFor="search">By country: </label>
+            <input
+              type="text"
+              name="search"
+              id="search"
+              value={searchValue}
+              onChange={handleSearchInput}
+            />
+
+            <label htmlFor="continent">By continent:</label>
+            <select
+              name="continent"
+              id="continent"
+              value={continentSelect}
+              onChange={handleSelectContinent}
+            >
+              <option value="all">All</option>
+              <option value="africa">Africa</option>
+              <option value="north-america">North America</option>
+              <option value="south-america">South America</option>
+              <option value="asia">Asia</option>
+              <option value="europe">Europe</option>
+              <option value="oceania">Oceania</option>
+            </select>
+          </form>
         </div>
         <ul>{renderCountryCards()}</ul>
       </main>
