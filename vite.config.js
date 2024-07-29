c5083793493bf7beca78f3b27c9b392023600cc3
Irene García <irenegwodak@gmail.com>
2024-07-29 10:34:04 +0200

    update vite base link

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/modulo-3-evaluacion-intermedia-irenegwodak/"
+  base: "/countries-finder-api-react/"
 })
